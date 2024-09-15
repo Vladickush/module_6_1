94106d8174120e2db9385b50786fcf5b249287fc
Vladickush <u.vn@mail.ru>
2024-09-15 17:46:36 +0300

    Update module_6_3.py

--- module_6_3.py
+++ module_6_3.py
@@ -1,21 +1,26 @@
 # Задача "Мифическое наследование":
 class Horse:
-    x_distance = 0  # пройденный путь.
-    sound = 'Frrr'  # звук, который издаёт лошадь.
+    def __init__(self, x_distance=0, sound='Frrr'):
+        self.x_distance = x_distance
+        self.sound = sound
 
     def run(self, dx):
         self.x_distance += dx
 
 
-class Eagle:
-    y_distance = 0  # высота полёта
-    sound = 'I train, eat, sleep, and repeat'  # звук, который издаёт орёл
+class Eagle():
+    def __init__(self, y_distance=0, sound='I train, eat, sleep, and repeat'):
+        self.y_distance = y_distance  # высота полёта
+        self.sound = sound
 
     def fly(self, dy):
         self.y_distance += dy
 
 
-class Pegasus(Eagle, Horse):
+class Pegasus(Horse, Eagle):
+    def __init__(self):
+        Horse.__init__(self)
+        Eagle.__init__(self)
 
     def move(self, dx, dy):
         self.run(dx)
@@ -37,3 +42,4 @@ p1.move(-5, 20)
 print(p1.get_pos())
 
 p1.voice()
+
